afff7bf94e086769bbbf7adbc871eda513167fc9
vabby0666 <vaibhav.c@kustommade.com>
2018-10-31 16:30:44 +0530

    SpringMVC4 upload to github

---
+++ src/mvc/SampleInitializer.java
@@ -0,0 +1,26 @@
+package mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SampleInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return new Class[] {SampleConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		return new String[] {"/"};
+	}
+
+	
+}
